9853e547a614ecd6c9e7d5fdf4ea2b0add2b45e3
Miguel Lahoz <miguel.lahoz@gmail.com>
2016-01-05 10:50:13 +0100

    Unit tests for commands

--- src/game/command.rs
+++ src/game/command.rs
@@ -2,7 +2,7 @@ use std::fmt;
 
 pub enum Command {
     New { width: usize, height: usize},
-    Set { x: usize, y: usize},
+    Toggle { x: usize, y: usize},
     Play,
     Print,
     Quit
@@ -13,40 +13,28 @@ pub fn parse_command(input: &str) -> Result<Command, &str> {
 
     if tokens.len() != 0 {
 
-        // TODO: refactor parsing of arguments
-
         match tokens[0] {
-            "quit" => return Ok(Command::Quit),
-            "play" => return Ok(Command::Play),
-            "print" => return Ok(Command::Print),
-            "new" => {
+            "quit" | "q" => return Ok(Command::Quit),
+            "play" | "p" => return Ok(Command::Play),
+            "print" | "pr" => return Ok(Command::Print),
+            c @ "new" | c @ "n" | c @ "toggle" | c @ "t" => {
                 if tokens.len() == 3 {
-                    let width: usize = match tokens[1].parse() {
+                    let first: usize = match tokens[1].parse() {
                         Ok(num) => num,
-                        Err(_) => return Err("Invalid width value"),
+                        Err(_) => return Err("Invalid first argument value"),
                     };
-                    let height: usize = match tokens[2].parse() {
+                    let second: usize = match tokens[2].parse() {
                         Ok(num) => num,
-                        Err(_) => return Err("Invalid height value"),
+                        Err(_) => return Err("Invalid second argument value"),
                     };
-                    return Ok(Command::New{ width: width, height: height});
+
+                    match c {
+                        "new" | "n" => return Ok(Command::New{ width: first, height: second}),
+                        "toggle" | "t" => return Ok(Command::Toggle{ x: first, y: second}),
+                        _ => return Err("Invalid syntax for command"),
+                    }
                 } else {
-                    return Err("Invalid syntax for new command");
-                }
-            },
-            "set" => {
-                if tokens.len() == 3 {
-                    let x: usize = match tokens[1].parse() {
-                        Ok(num) => num,
-                        Err(_) => return Err("Invalid x value"),
-                    };
-                    let y: usize = match tokens[2].parse() {
-                        Ok(num) => num,
-                        Err(_) => return Err("Invalid y value"),
-                    };
-                    return Ok(Command::Set{ x: x, y: y});
-                } else {
-                    return Err("Invalid syntax for set command");
+                    return Err("Invalid syntax for command");
                 }
             },
             _ => return Err("Unknown command"),
@@ -65,10 +53,116 @@ impl fmt::Display for Command {
             Command::Play => write!(f, "Play"),
             Command::Print => write!(f, "Print"),
             Command::New { width, height } => write!(f, "New [width: {} height: {}]", width, height),
-            Command::Set { x, y } => write!(f, "Set [x: {} y: {}]", x, y),
+            Command::Toggle { x, y } => write!(f, "Toggle [x: {} y: {}]", x, y),
         }
 
     }
 }
 
-// TODO: unit tests
+#[test]
+fn command_new() {
+    let c = Command::New { width: 30, height: 20 };
+    assert_eq!(c.to_string(), "New [width: 30 height: 20]");
+
+    // parse
+    let pc = parse_command("new 30 20").ok().unwrap();
+    if let Command::New { width: 30, height: 20} = pc { assert!(true); } else { assert!(false); }
+
+    let pc = parse_command("new 20 80").ok().unwrap();
+    if let Command::New { width: 20, height: 80} = pc { assert!(true); } else { assert!(false); }
+
+    let pc = parse_command("n 30 20").ok().unwrap();
+    if let Command::New { width: 30, height: 20} = pc { assert!(true); } else { assert!(false); }
+
+    let error = parse_command("new thirty twenty").err().unwrap();
+    assert_eq!(error, "Invalid first argument value");
+
+    let error = parse_command("new 30 twenty").err().unwrap();
+    assert_eq!(error, "Invalid second argument value");
+
+    let error = parse_command("new 30").err().unwrap();
+    assert_eq!(error, "Invalid syntax for command");
+
+    let error = parse_command("new").err().unwrap();
+    assert_eq!(error, "Invalid syntax for command");
+
+    let error = parse_command("ne 30 20").err().unwrap();
+    assert_eq!(error, "Unknown command");
+}
+
+#[test]
+fn command_toggle() {
+    let c = Command::Toggle { x: 4, y: 7 };
+    assert_eq!(c.to_string(), "Toggle [x: 4 y: 7]");
+
+    // parse
+    let pc = parse_command("toggle 4 7").ok().unwrap();
+    if let Command::Toggle { x: 4, y: 7} = pc { assert!(true); } else { assert!(false); }
+
+    let pc = parse_command("toggle 2 8").ok().unwrap();
+    if let Command::Toggle { x: 2, y: 8} = pc { assert!(true); } else { assert!(false); }
+
+    let pc = parse_command("t 4 7").ok().unwrap();
+    if let Command::Toggle { x: 4, y: 7} = pc { assert!(true); } else { assert!(false); }
+
+    let error = parse_command("toggle four seven").err().unwrap();
+    assert_eq!(error, "Invalid first argument value");
+
+    let error = parse_command("toggle 4 seven").err().unwrap();
+    assert_eq!(error, "Invalid second argument value");
+
+    let error = parse_command("toggle 4").err().unwrap();
+    assert_eq!(error, "Invalid syntax for command");
+
+    let error = parse_command("toggle").err().unwrap();
+    assert_eq!(error, "Invalid syntax for command");
+
+    let error = parse_command("tog 4 7").err().unwrap();
+    assert_eq!(error, "Unknown command");
+}
+
+#[test]
+fn command_play() {
+    let c = Command::Play;
+    assert_eq!(c.to_string(), "Play");
+
+    let pc = parse_command("play").ok().unwrap();
+    if let Command::Play = pc { assert!(true); } else { assert!(false); }
+
+    let pc = parse_command("p").ok().unwrap();
+    if let Command::Play = pc { assert!(true); } else { assert!(false); }
+
+    let error = parse_command("pla").err().unwrap();
+    assert_eq!(error, "Unknown command");
+
+}
+
+#[test]
+fn command_print() {
+    let c = Command::Print;
+    assert_eq!(c.to_string(), "Print");
+
+    let pc = parse_command("print").ok().unwrap();
+    if let Command::Print = pc { assert!(true); } else { assert!(false); }
+
+    let pc = parse_command("pr").ok().unwrap();
+    if let Command::Print = pc { assert!(true); } else { assert!(false); }
+
+    let error = parse_command("pri").err().unwrap();
+    assert_eq!(error, "Unknown command");
+}
+
+#[test]
+fn command_quit() {
+    let c = Command::Quit;
+    assert_eq!(c.to_string(), "Quit");
+
+    let pc = parse_command("quit").ok().unwrap();
+    if let Command::Quit = pc { assert!(true); } else { assert!(false); }
+
+    let pc = parse_command("q").ok().unwrap();
+    if let Command::Quit = pc { assert!(true); } else { assert!(false); }
+
+    let error = parse_command("qui").err().unwrap();
+    assert_eq!(error, "Unknown command");
+}
