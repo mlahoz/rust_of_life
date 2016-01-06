ffb3f70ee4c57958292ad6b218c6d7d87dacf75a
Miguel Lahoz <miguel.lahoz@gmail.com>
2016-01-06 20:54:14 +0100

    Unit tests for engine

--- src/game/engine.rs
+++ src/game/engine.rs
@@ -25,7 +25,11 @@ impl Engine {
     }
 
     fn play(&mut self) -> Result<(), &str> {
-        Err("TODO!")
+        if self.board.is_some() {
+            Err("TODO!")
+        } else {
+            Err("Game not created yet")
+        }
     }
 
     pub fn process_command(&mut self, command: command::Command) -> Result<(), &str> {
@@ -44,4 +48,93 @@ impl Engine {
             command::Command::Toggle { x, y } => self.toggle(x, y),
         }
     }
+}
+
+#[test]
+fn engine_constructor() {
+    let e = Engine::new();
+    assert!(e.board.is_none());
+}
+
+#[test]
+fn engine_board() {
+    let e = Engine::new();
+    let b = e.board();
+    assert!(b.is_none());
+}
+
+#[test]
+fn engine_cmd_new() {
+    let mut e = Engine::new();
+    assert!(e.process_command(command::Command::New { width: 10, height: 10 } ).is_ok());
+
+    let b = e.board();
+    assert!(b.is_some());
+    let b = e.board().unwrap();
+    assert_eq!(b.width, 10);
+    assert_eq!(b.height, 10);
+
+    assert!(e.process_command(command::Command::New { width: 30, height: 20 } ).is_ok());
+    let b = e.board().unwrap();
+    assert_eq!(b.width, 30);
+    assert_eq!(b.height, 20);
+}
+
+#[test]
+fn engine_cmd_quit() {
+    let mut e = Engine::new();
+    assert!(e.process_command(command::Command::Quit).is_ok());
+
+    assert!(e.process_command(command::Command::New { width: 10, height: 10 } ).is_ok());
+    assert!(e.process_command(command::Command::Quit).is_ok());
+
+    assert!(e.board.is_none());
+}
+
+#[test]
+fn engine_toggle() {
+    let mut e = Engine::new();
+    assert!(e.toggle(3, 7).is_err());
+    assert_eq!(e.toggle(3, 7).err().unwrap(), "Game not created yet");
+
+    assert!(e.process_command(command::Command::New { width: 10, height: 10 } ).is_ok());
+    let b = e.board().unwrap();
+    assert_eq!(b.get(2, 6).ok().unwrap(), false);
+    assert!(e.toggle(3, 7).is_ok());
+    let b = e.board().unwrap();
+    assert_eq!(b.get(2, 6).ok().unwrap(), true);
+}
+
+#[test]
+fn engine_play() {
+    let mut e = Engine::new();
+    assert!(e.play().is_err());
+    assert_eq!(e.play().err().unwrap(), "Game not created yet");
+
+    assert!(e.process_command(command::Command::New { width: 10, height: 10 } ).is_ok());
+    assert!(e.play().is_err());
+}
+
+#[test]
+fn engine_cmd_toggle() {
+    let mut e = Engine::new();
+    assert!(e.process_command(command::Command::Toggle { x: 3, y: 7 } ).is_err());
+    assert_eq!(e.process_command(command::Command::Toggle { x: 3, y: 7 } ).err().unwrap(), "Game not created yet");
+
+    assert!(e.process_command(command::Command::New { width: 10, height: 10 } ).is_ok());
+    let b = e.board().unwrap();
+    assert_eq!(b.get(2, 6).ok().unwrap(), false);
+    assert!(e.process_command(command::Command::Toggle { x: 3, y: 7 } ).is_ok());
+    let b = e.board().unwrap();
+    assert_eq!(b.get(2, 6).ok().unwrap(), true);
+}
+
+#[test]
+fn engine_cmd_play() {
+    let mut e = Engine::new();
+    assert!(e.process_command(command::Command::Play).is_err());
+    assert_eq!(e.process_command(command::Command::Play).err().unwrap(), "Game not created yet");
+
+    assert!(e.process_command(command::Command::New { width: 10, height: 10 } ).is_ok());
+    assert!(e.process_command(command::Command::Play).is_err());
 }
