111546b694da9e48edfad47c002a8eb2fcd34d0b
Miguel Lahoz <miguel.lahoz@gmail.com>
2016-01-07 15:11:06 +0100

    Unit tests for game logic

--- src/game/engine.rs
+++ src/game/engine.rs
@@ -140,16 +140,6 @@ fn engine_toggle() {
 }
 
 #[test]
-fn engine_play() {
-    let mut e = Engine::new();
-    assert!(e.play().is_err());
-    assert_eq!(e.play().err().unwrap(), "Game not created yet");
-
-    assert!(e.process_command(command::Command::New { width: 10, height: 10 } ).is_ok());
-    assert!(e.play().is_ok());
-}
-
-#[test]
 fn engine_cmd_toggle() {
     let mut e = Engine::new();
     assert!(e.process_command(command::Command::Toggle { x: 3, y: 7 } ).is_err());
@@ -171,4 +161,293 @@ fn engine_cmd_play() {
 
     assert!(e.process_command(command::Command::New { width: 10, height: 10 } ).is_ok());
     assert!(e.process_command(command::Command::Play).is_ok());
+}
+
+#[test]
+fn engine_play() {
+    let mut e = Engine::new();
+    assert!(e.play().is_err());
+    assert_eq!(e.play().err().unwrap(), "Game not created yet");
+
+    assert!(e.process_command(command::Command::New { width: 6, height: 6 } ).is_ok());
+    assert!(e.play().is_ok());
+    let b = e.board().unwrap();
+
+    let expected = "______\n\
+                    ______\n\
+                    ______\n\
+                    ______\n\
+                    ______\n\
+                    ______\n";
+
+    assert_eq!(b.to_string(), expected);
+}
+
+#[test]
+fn engine_play_starve_underpopulation() {
+    let mut e = Engine::new();
+
+    assert!(e.process_command(command::Command::New { width: 3, height: 3 } ).is_ok());
+
+    // starve 0 neighbours
+    assert!(e.toggle(1, 1).is_ok());
+    assert!(e.play().is_ok());
+
+    let b = e.board().unwrap();
+    assert_eq!(b.get(0,0).unwrap(), false);
+
+    // starve 1 neighbour
+    assert!(e.toggle(1, 1).is_ok());
+    assert!(e.toggle(2, 1).is_ok());
+    assert!(e.play().is_ok());
+
+    let b = e.board().unwrap();
+    assert_eq!(b.get(0,0).unwrap(), false);
+}
+
+#[test]
+fn engine_play_survive() {
+    let mut e = Engine::new();
+
+    // survive 2 neighbours
+    assert!(e.process_command(command::Command::New { width: 3, height: 3 } ).is_ok());
+    assert!(e.toggle(1, 1).is_ok());
+    assert!(e.toggle(2, 1).is_ok());
+    assert!(e.toggle(3, 1).is_ok());
+    assert!(e.play().is_ok());
+
+    let b = e.board().unwrap();
+    assert_eq!(b.get(1,0).unwrap(), true);
+
+    // survive 3 neighbours
+    assert!(e.process_command(command::Command::New { width: 3, height: 3 } ).is_ok());
+    assert!(e.toggle(1, 1).is_ok());
+    assert!(e.toggle(2, 1).is_ok());
+    assert!(e.toggle(3, 1).is_ok());
+    assert!(e.toggle(2, 2).is_ok());
+    assert!(e.play().is_ok());
+
+    let b = e.board().unwrap();
+    assert_eq!(b.get(1,1).unwrap(), true);
+}
+
+#[test]
+fn engine_play_starve_overpopulation() {
+    let mut e = Engine::new();
+
+    // starve 4 neighbours
+    assert!(e.process_command(command::Command::New { width: 3, height: 3 } ).is_ok());
+    assert!(e.toggle(1, 1).is_ok());
+    assert!(e.toggle(2, 1).is_ok());
+    assert!(e.toggle(3, 1).is_ok());
+    assert!(e.toggle(1, 2).is_ok());
+    assert!(e.toggle(2, 2).is_ok());
+    assert!(e.play().is_ok());
+
+    let b = e.board().unwrap();
+    assert_eq!(b.get(1,0).unwrap(), false);
+
+    // starve 5 neighbours
+    assert!(e.process_command(command::Command::New { width: 3, height: 3 } ).is_ok());
+    assert!(e.toggle(1, 1).is_ok());
+    assert!(e.toggle(2, 1).is_ok());
+    assert!(e.toggle(3, 1).is_ok());
+    assert!(e.toggle(1, 2).is_ok());
+    assert!(e.toggle(2, 2).is_ok());
+    assert!(e.toggle(3, 2).is_ok());
+    assert!(e.play().is_ok());
+
+    let b = e.board().unwrap();
+    assert_eq!(b.get(1,0).unwrap(), false);
+
+    // starve 6 neighbours
+    assert!(e.process_command(command::Command::New { width: 3, height: 3 } ).is_ok());
+    assert!(e.toggle(1, 1).is_ok());
+    assert!(e.toggle(2, 1).is_ok());
+    assert!(e.toggle(3, 1).is_ok());
+    assert!(e.toggle(1, 2).is_ok());
+    assert!(e.toggle(2, 2).is_ok());
+    assert!(e.toggle(3, 2).is_ok());
+    assert!(e.toggle(1, 3).is_ok());
+    assert!(e.play().is_ok());
+
+    let b = e.board().unwrap();
+    assert_eq!(b.get(1,0).unwrap(), false);
+
+    // starve 7 neighbours
+    assert!(e.process_command(command::Command::New { width: 3, height: 3 } ).is_ok());
+    assert!(e.toggle(1, 1).is_ok());
+    assert!(e.toggle(2, 1).is_ok());
+    assert!(e.toggle(3, 1).is_ok());
+    assert!(e.toggle(1, 2).is_ok());
+    assert!(e.toggle(2, 2).is_ok());
+    assert!(e.toggle(3, 2).is_ok());
+    assert!(e.toggle(1, 3).is_ok());
+    assert!(e.toggle(2, 3).is_ok());
+    assert!(e.play().is_ok());
+
+    let b = e.board().unwrap();
+    assert_eq!(b.get(1,0).unwrap(), false);
+
+    // starve 8 neighbours
+    assert!(e.process_command(command::Command::New { width: 3, height: 3 } ).is_ok());
+    assert!(e.toggle(1, 1).is_ok());
+    assert!(e.toggle(2, 1).is_ok());
+    assert!(e.toggle(3, 1).is_ok());
+    assert!(e.toggle(1, 2).is_ok());
+    assert!(e.toggle(2, 2).is_ok());
+    assert!(e.toggle(3, 2).is_ok());
+    assert!(e.toggle(1, 3).is_ok());
+    assert!(e.toggle(2, 3).is_ok());
+    assert!(e.toggle(3, 3).is_ok());
+    assert!(e.play().is_ok());
+
+    let b = e.board().unwrap();
+    assert_eq!(b.get(1,0).unwrap(), false);
+}
+
+#[test]
+fn engine_play_born() {
+    let mut e = Engine::new();
+
+    // 1 neighbour no born
+    assert!(e.process_command(command::Command::New { width: 3, height: 3 } ).is_ok());
+    assert!(e.toggle(1, 1).is_ok());
+    assert!(e.play().is_ok());
+
+    let b = e.board().unwrap();
+    assert_eq!(b.get(1,1).unwrap(), false);
+
+    // 2 neighbours no born
+    assert!(e.process_command(command::Command::New { width: 3, height: 3 } ).is_ok());
+    assert!(e.toggle(1, 1).is_ok());
+    assert!(e.toggle(2, 1).is_ok());
+    assert!(e.play().is_ok());
+
+    let b = e.board().unwrap();
+    assert_eq!(b.get(1,1).unwrap(), false);
+
+    // 3 neighbours new born!
+    assert!(e.process_command(command::Command::New { width: 3, height: 3 } ).is_ok());
+    assert!(e.toggle(1, 1).is_ok());
+    assert!(e.toggle(2, 1).is_ok());
+    assert!(e.toggle(3, 1).is_ok());
+    assert!(e.play().is_ok());
+
+    let b = e.board().unwrap();
+    assert_eq!(b.get(1,1).unwrap(), true);
+
+    // 4 neighbours no born
+    assert!(e.process_command(command::Command::New { width: 3, height: 3 } ).is_ok());
+    assert!(e.toggle(1, 1).is_ok());
+    assert!(e.toggle(2, 1).is_ok());
+    assert!(e.toggle(3, 1).is_ok());
+    assert!(e.toggle(1, 2).is_ok());
+    assert!(e.play().is_ok());
+
+    let b = e.board().unwrap();
+    assert_eq!(b.get(1,1).unwrap(), false);
+
+    // 5 neighbours no born
+    assert!(e.process_command(command::Command::New { width: 3, height: 3 } ).is_ok());
+    assert!(e.toggle(1, 1).is_ok());
+    assert!(e.toggle(2, 1).is_ok());
+    assert!(e.toggle(3, 1).is_ok());
+    assert!(e.toggle(1, 2).is_ok());
+    assert!(e.toggle(3, 2).is_ok());
+    assert!(e.play().is_ok());
+
+    let b = e.board().unwrap();
+    assert_eq!(b.get(1,1).unwrap(), false);
+
+    // 6 neighbours no born
+    assert!(e.process_command(command::Command::New { width: 3, height: 3 } ).is_ok());
+    assert!(e.toggle(1, 1).is_ok());
+    assert!(e.toggle(2, 1).is_ok());
+    assert!(e.toggle(3, 1).is_ok());
+    assert!(e.toggle(1, 2).is_ok());
+    assert!(e.toggle(3, 2).is_ok());
+    assert!(e.toggle(1, 3).is_ok());
+    assert!(e.play().is_ok());
+
+    let b = e.board().unwrap();
+    assert_eq!(b.get(1,1).unwrap(), false);
+
+    // 7 neighbours no born
+    assert!(e.process_command(command::Command::New { width: 3, height: 3 } ).is_ok());
+    assert!(e.toggle(1, 1).is_ok());
+    assert!(e.toggle(2, 1).is_ok());
+    assert!(e.toggle(3, 1).is_ok());
+    assert!(e.toggle(1, 2).is_ok());
+    assert!(e.toggle(3, 2).is_ok());
+    assert!(e.toggle(1, 3).is_ok());
+    assert!(e.toggle(2, 3).is_ok());
+    assert!(e.play().is_ok());
+
+    let b = e.board().unwrap();
+    assert_eq!(b.get(1,1).unwrap(), false);
+
+    // 8 neighbours no born
+    assert!(e.process_command(command::Command::New { width: 3, height: 3 } ).is_ok());
+    assert!(e.toggle(1, 1).is_ok());
+    assert!(e.toggle(2, 1).is_ok());
+    assert!(e.toggle(3, 1).is_ok());
+    assert!(e.toggle(1, 2).is_ok());
+    assert!(e.toggle(3, 2).is_ok());
+    assert!(e.toggle(1, 3).is_ok());
+    assert!(e.toggle(2, 3).is_ok());
+    assert!(e.toggle(3, 3).is_ok());
+    assert!(e.play().is_ok());
+
+    let b = e.board().unwrap();
+    assert_eq!(b.get(1,1).unwrap(), false);
+}
+
+#[test]
+fn engine_play_blinker() {
+    let mut e = Engine::new();
+
+    assert!(e.process_command(command::Command::New { width: 3, height: 3 } ).is_ok());
+    assert!(e.toggle(1, 2).is_ok());
+    assert!(e.toggle(2, 2).is_ok());
+    assert!(e.toggle(3, 2).is_ok());
+
+    let expected_1 = "_X_\n\
+                      _X_\n\
+                      _X_\n";
+    let expected_2 = "___\n\
+                      XXX\n\
+                      ___\n";
+
+    assert!(e.play().is_ok());
+    let b = e.board().unwrap();
+    assert_eq!(b.to_string(), expected_1);
+
+    assert!(e.play().is_ok());
+    let b = e.board().unwrap();
+    assert_eq!(b.to_string(), expected_2);
+}
+
+#[test]
+fn engine_play_block() {
+    let mut e = Engine::new();
+
+    assert!(e.process_command(command::Command::New { width: 4, height: 4 } ).is_ok());
+    assert!(e.toggle(2, 2).is_ok());
+    assert!(e.toggle(2, 3).is_ok());
+    assert!(e.toggle(3, 2).is_ok());
+    assert!(e.toggle(3, 3).is_ok());
+
+    let expected = "____\n\
+                    _XX_\n\
+                    _XX_\n\
+                    ____\n";
+
+    assert!(e.play().is_ok());
+    let b = e.board().unwrap();
+    assert_eq!(b.to_string(), expected);
+
+    assert!(e.play().is_ok());
+    let b = e.board().unwrap();
+    assert_eq!(b.to_string(), expected);
 }
